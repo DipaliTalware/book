bce69beb1472de49ba2d9718fff0c8d0d346bd7d
DipaliTalware <d.talware@gmail.com>
2024-09-05 10:45:14 +0200

    Step 3, 4 and 5 done.

--- src/main/java/org/newfische/recapproject/Main.java
+++ src/main/java/org/newfische/recapproject/Main.java
@@ -5,5 +5,11 @@ public class Main {
         Book book1 = new Book("Jeffrey Archer", "The Grass is Always Greener", "1-86092-049-7");
         System.out.println(book1.toString());
 
+        Book book2 = new Book("The Great Switcheroo","Roald Dahl","1-86092-034-9");
+
+        Book[] books = {book1, book2};
+        Library library = new Library(books);
+        System.out.println(library.toString());
+
     }
 }
